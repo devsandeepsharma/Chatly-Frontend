bdfbea906a46805c601d6d40d8c044838cc16964
Sandeep <sandipsharma2445@gmail.com>
2025-09-03 10:18:13 +0530

    project setup using tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <h1 className="text-4xl font-bold">Chatly: Real Time Chat Application</h1>
+        </div>
+    )
+}
+
+export default App;
