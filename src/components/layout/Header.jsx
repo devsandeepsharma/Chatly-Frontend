c67431f9454a0dc9d41e9d66f21667763e1caf12
Sandeep <sandipsharma2445@gmail.com>
2025-09-04 16:46:27 +0530

    header added

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,41 @@
+import { Search } from "lucide-react";
+import Logo from "../ui/Logo";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+
+    const { user } = useSelector(state => state.auth);
+
+    return (
+        <header className="sticky top-0 z-40 h-16 bg-white/70 border-b border-gray-200/80">
+            <div className="p-4 w-full max-w-7xl mx-auto flex items-center justify-between">
+                <Logo />
+                <nav className="flex justify-center items-center gap-4" aria-label="primary navigation">
+                    <button className="inline-flex items-center justify-center gap-2 text-sm text-gray-700 px-4 py-2 hover:text-[#00BFA6] transition-colors cursor-pointer">
+                        <Search className="h-4 w-4" /> Search
+                    </button>
+                    <button
+                        className="w-8 h-8 rounded-full overflow-hidden border-1 border-[#00BFA6]/50 shadow-sm hover:shadow-md transition"
+                        aria-label="Profile"
+                    >
+                        {
+                            user?.avatar ? (
+                                <img
+                                    src={user?.avatar}
+                                    alt={user?.username || "User Avatar"}
+                                    className="w-full h-full object-cover"
+                                />
+                            ) : (
+                                <div className="w-full h-full bg-gray-300 flex items-center justify-center text-gray-600 text-sm font-medium">
+                                    {user?.username?.[0]?.toUpperCase() || "U"}
+                                </div>
+                            )
+                        }
+                    </button>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 
+import Header from "../components/layout/Header";
+
 const Chats = () => {
 
     const { user } = useSelector(state => state.auth);
 
     return (
         <>
-            <img width={200} height={200} src={user?.avatar} />
-            <h1>Hello {user?.username} 👋</h1>
+            <Header />
+            <div className="w-sm mx-auto mt-5 border border-gray-200">
+                <img width={200} height={200} src={user?.avatar} />
+                <h1>Hello {user?.username} 👋</h1>
+            </div>
         </>
     )
 }
